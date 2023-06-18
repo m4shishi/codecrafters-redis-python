9cb3984c28b5c023d50f89445fda157547d32ca5
Popcorn Dreamer <bloom.cyberix@gmail.com>
2023-06-18 18:01:21 +0400

    task

--- app/main.py
+++ app/main.py
@@ -11,8 +11,9 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, addr = server_socket.accept() # wait for client
     with conn:
-        data = conn.recv(1024)
-        conn.sendall(b"+PONG\r\n")
+        while True:
+            data = conn.recv(1024)
+            conn.sendall(bytes(f"+{data}\r\n"))
 
 
 if __name__ == "__main__":
